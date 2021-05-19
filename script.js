046c9f66325fef93608af9a284aab6ef06a014ff
Lucas Alves <as.lucasalves@gmail.com>
2021-05-18 22:53:34 -0300

    reorganizei a nav bar header

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// const target = document.querySelectorAll('[data-anime]')
+// const animationClass = "animate"
+
+// function animeScroll(){
+//     // const windowTop = window.pageYOffset +600;
+//     // target.forEach(function(e){
+//     //     if((windowTop)> e.offsetTop){
+//     //         e.classList.add(animationClass);
+//     //     }
+//     // })
+//      alert("oi")
+// }
+
+// document.addEventListener('scroll',()=>{
+//     alert('oi')
+// })
+    
+
